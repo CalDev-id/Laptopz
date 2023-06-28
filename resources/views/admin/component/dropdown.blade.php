8a2c1b4ea43d67e932979fad5b2f069c1fb6b2e8
Dynavx <haical31@gmail.com>
2023-06-28 15:24:02 +0700

    update

--- resources/views/admin/component/dropdown.blade.php
+++ resources/views/admin/component/dropdown.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="row">
             <div class="col-12 col-md-6 order-md-1 order-last">
-                <h3>dropdown</h3>
+                <h3>dropdowna</h3>
             </div>
             <div class="col-12 col-md-6 order-md-2 order-first">
                 <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
