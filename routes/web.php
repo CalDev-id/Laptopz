cae524da6c018892cfa9f4791f6e0d18c6293770
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-07-10 16:45:28 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KriteriaController;
+use App\Http\Controllers\SubkriteriaController;
+use App\Http\Controllers\AlternatifController;
+use App\Http\Controllers\PenilaianController;
+use App\Http\Controllers\PerhitunganController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +29,24 @@ Route::get('/dashboard', function () {
         'bodyClass' => 'hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed'
     ]);
 });
+
+Route::resource('kriteria', KriteriaController::class)->except(['show']);
+Route::get('kriteria/{id}', [KriteriaController::class, 'display'])->name('kriteria.display');
+
+Route::resource('subkriteria', SubkriteriaController::class)->except(['show']);
+
+Route::resource('alternatif', AlternatifController::class)->except(['show']);
+
+Route::resource('penilaian', PenilaianController::class)->except(['show']);
+
+Route::get('/perhitungan', [PerhitunganController::class, 'index'])->name('perhitungan.index');
+// Route::get('/kriteria', function() {
+//     session(['dark-mode' => false]);
+//     return view('kriteria.index', [
+//         'title' => 'Kriteria',
+//         'bodyClass' => 'hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed'
+//     ]);
+// });
 
 Route::get('/test', function () {
     session(['dark-mode' => true]);
