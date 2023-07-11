109927ba2822aa4ae224e47444a6b6f976f466c9
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-07-12 01:58:23 +0700

    Update sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -16,7 +16,7 @@
 		<nav class="mt-2">
 			<ul class="nav nav-pills nav-sidebar flex-column" data-accordion="false" data-widget="treeview" role="menu">
 				<li class="nav-item">
-					<a class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}" href="/dashboard">
+					<a class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}" href="{{ route('dashboard.index') }}">
 						<i class="nav-icon fas fa-tachometer-alt"></i>
 						<p>Dashboard</p>
 					</a>
