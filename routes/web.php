c2ca1bb9a33ae8ff1902be0f8644d5efd49fd368
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-07-12 01:57:52 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KriteriaController;
 use App\Http\Controllers\SubkriteriaController;
 use App\Http\Controllers\AlternatifController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PenilaianController;
 use App\Http\Controllers\PerhitunganController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,13 +24,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    session(['dark-mode' => false]);
-    return view('dashboard.index', [
-        'title' => 'Dashboard',
-        'bodyClass' => 'hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed'
-    ]);
-});
+Route::resource('dashboard', DashboardController::class)->except(['show']);
 
 Route::resource('kriteria', KriteriaController::class)->except(['show']);
 Route::get('kriteria/{id}', [KriteriaController::class, 'display'])->name('kriteria.display');
@@ -40,13 +36,6 @@ Route::resource('alternatif', AlternatifController::class)->except(['show']);
 Route::resource('penilaian', PenilaianController::class)->except(['show']);
 
 Route::get('/perhitungan', [PerhitunganController::class, 'index'])->name('perhitungan.index');
-// Route::get('/kriteria', function() {
-//     session(['dark-mode' => false]);
-//     return view('kriteria.index', [
-//         'title' => 'Kriteria',
-//         'bodyClass' => 'hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed'
-//     ]);
-// });
 
 Route::get('/test', function () {
     session(['dark-mode' => true]);
