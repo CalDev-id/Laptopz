86f3294921f4cc5fded40395426ba853cca4a403
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-07-12 01:58:43 +0700

    Create DashboardController.php

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Kriteria;
+use App\Models\Subkriteria;
+use App\Models\Alternatif;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    public function index()
+    {
+        session(['dark-mode' => false]);
+        $data['title'] = 'Dashboard';
+        $data['user'] = User::count();
+        $data['kriteria'] = Kriteria::count();
+        $data['subkriteria'] = Subkriteria::count();
+        $data['alternatif'] = Alternatif::count();
+        $data['bodyClass'] = 'hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed';
+        return view('dashboard.index', $data);
+    }
+}
