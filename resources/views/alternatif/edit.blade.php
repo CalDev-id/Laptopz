8ca6ea15fa6d839789b53907b246a190c5be8d6c
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-07-12 01:58:34 +0700

    Update edit.blade.php

--- resources/views/alternatif/edit.blade.php
+++ resources/views/alternatif/edit.blade.php
@@ -25,8 +25,8 @@
                             </div>
                         </div>
                         <div class="card-footer">
-                            <button type="submit" class="btn btn-primary">Simpan</button>
-                            <a href="{{ route('alternatif.index') }}" class="btn btn-success float-right">Kembali</a>
+                            <button type="submit" class="btn btn-primary"><i class="fas fa-save mr-1"></i> Simpan</button>
+                            <a href="{{ route('alternatif.index') }}" class="btn btn-success float-right"><i class="fas fa-arrow-left mr-1"></i> Kembali</a>
                         </div>
                     </form>
                 </div>
@@ -55,12 +55,12 @@
                                             <td>
                                                 <div class="d-flex justify-content-center">
                                                     <div class="btn-group">
-                                                        <a href="{{ route('alternatif.edit', $row->id) }}" class="btn btn-warning rounded mr-2"><i class="fa fa-edit"></i></a>
+                                                        <a href="{{ route('alternatif.edit', $row->id) }}" class="btn btn-warning rounded mr-2"><i class="fa fa-edit mr-1"></i> Edit</a>
                                                         <form action="{{ route('alternatif.destroy', $row->id) }}" method="POST" class="delete-form">
                                                             @csrf
                                                             @method('DELETE')
                                                             <button type="submit" class="btn btn-danger rounded hapus">
-                                                                <i class="fa fa-trash"></i>
+                                                                <i class="fa fa-trash mr-1"></i> Hapus
                                                             </button>
                                                         </form>
                                                     </div>
