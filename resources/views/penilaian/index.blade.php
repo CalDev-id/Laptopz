6e17e42c4dcd5ee0c3281ae83ecf04dcf8486830
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-07-12 02:11:16 +0700

    Update index.blade.php

--- resources/views/penilaian/index.blade.php
+++ resources/views/penilaian/index.blade.php
@@ -81,7 +81,7 @@
                                             <div class="modal-dialog" role="document">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
-                                                        <h5 class="modal-title" id="alternativeModalLabel{{ $valt->id }}">Nama Alternatif: {{ $valt->nama_alternatif }}</h5>
+                                                        <h5 class="modal-title" id="alternativeModalLabel{{ $valt->id }}">Nama Alternatif : {{ $valt->nama_alternatif }}</h5>
                                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                             <span aria-hidden="true">&times;</span>
                                                         </button>
