f7b0487e12ee60415b8fbfa66b9a4a109e2b6608
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-07-12 23:49:46 +0700

    Update index.blade.php

--- resources/views/kriteria/index.blade.php
+++ resources/views/kriteria/index.blade.php
@@ -3,61 +3,105 @@
 @section('inner-content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-4 col-12">
-                <div class="card">
+            <div class="col-12">
+                <div class="card ">
                     <div class="card-header">
-                        <h3 class="card-title">Tambah Kriteria</h3>
+                        <h3 class="card-title">Preset Kriteria</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                        </div>
                     </div>
-                    <form action="{{ route('kriteria.store') }}" method="POST">
-                        @csrf
-                        <div class="card-body">
-                            <div class="form-group">
-                                <label for="kode">Kode Kriteria</label>
-                                <input type="text" class="form-control" id="kode" name="kode" required>
-                                @error('kode')
-                                    <div class="invalid-feedback" role="alert">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <label for="nama_kriteria">Nama Kriteria</label>
-                                <input type="text" class="form-control" id="nama_kriteria" name="nama_kriteria" required>
-                                @error('nama_kriteria')
-                                    <div class="invalid-feedback" role="alert">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <label for="attribut">Attribut Kriteria</label>
-                                <select name="attribut" class="form-control" id="attribut" required>
-                                    <option>Benefit</option>
-                                    <option>Cost</option>
+                    <div class="card-body">
+                        <form action="{{ route('kriteria.applyPreset') }}" method="POST">
+                            @csrf
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <button type="button" class="btn btn-secondary">Edit Preset</button>
+                                </div>
+                                <select name="preset" class="form-control rounded-right" id="preset" required>
+                                    <option value="allrounder" {{ $lastPreset === 'allrounder' ? 'selected' : '' }}>All Rounder</option>
+                                    <option value="graphicdesign" {{ $lastPreset === 'graphicdesign' ? 'selected' : '' }}>Editing / Graphic Design</option>
+                                    <option value="gamedevelopment" {{ $lastPreset === 'gamedevelopment' ? 'selected' : '' }}>Gaming / Game Development</option>
+                                    <option value="work/business" {{ $lastPreset === 'work/business' ? 'selected' : '' }}>Working / Business</option>
                                 </select>
-                                @error('attribut')
-                                    <div class="invalid-feedback" role="alert">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
+                                <button class="btn btn-success ml-3"><i class="fa fa-check mr-1"></i> Terapkan</button>
                             </div>
-                            <div class="form-group">
-                                <label for="bobot">Bobot Kriteria (%)</label>
-                                <input type="number" min="1" class="form-control" id="bobot" name="bobot" required>
-                                @error('bobot')
-                                    <div class="invalid-feedback" role="alert">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
+                        </form>
+                        <hr style="border-top: 3px dashed #ccc;">
+                        <div class="table-responsive">
+                            <table id="listpreset" class="table table-striped table-hover">
+                                <thead>
+                                    <tr class="bg-gradient bg-dark text-light">
+                                        <th>Nama Preset / Bobot Kriteria</th>
+                                        <th class="text-center">Kapasitas RAM</th>
+                                        <th class="text-center">Jenis Processor</th>
+                                        <th class="text-center">Core & Thread Processor</th>
+                                        <th class="text-center">Kapasitas Penyimpanan</th>
+                                        <th class="text-center">Jenis Penyimpanan</th>
+                                        <th class="text-center">Resolusi Layar</th>
+                                        <th class="text-center">Berat</th>
+                                        <th class="text-center">Kapasitas Baterai</th>
+                                        <th class="text-center">Harga</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>All Rounder</td>
+                                        <td class="p-1 align-middle text-center">13%</td>
+                                        <td class="p-1 align-middle text-center">8%</td>
+                                        <td class="p-1 align-middle text-center">15%</td>
+                                        <td class="p-1 align-middle text-center">13%</td>
+                                        <td class="p-1 align-middle text-center">13%</td>
+                                        <td class="p-1 align-middle text-center">10%</td>
+                                        <td class="p-1 align-middle text-center">8%</td>
+                                        <td class="p-1 align-middle text-center">10%</td>
+                                        <td class="p-1 align-middle text-center">10%</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Working / Business</td>
+                                        <td class="p-1 align-middle text-center">10%</td>
+                                        <td class="p-1 align-middle text-center">8%</td>
+                                        <td class="p-1 align-middle text-center">10%</td>
+                                        <td class="p-1 align-middle text-center">15%</td>
+                                        <td class="p-1 align-middle text-center">13%</td>
+                                        <td class="p-1 align-middle text-center">8%</td>
+                                        <td class="p-1 align-middle text-center">13%</td>
+                                        <td class="p-1 align-middle text-center">13%</td>
+                                        <td class="p-1 align-middle text-center">10%</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Gaming / Game Development</td>
+                                        <td class="p-1 align-middle text-center">13%</td>
+                                        <td class="p-1 align-middle text-center">10%</td>
+                                        <td class="p-1 align-middle text-center">20%</td>
+                                        <td class="p-1 align-middle text-center">13%</td>
+                                        <td class="p-1 align-middle text-center">13%</td>
+                                        <td class="p-1 align-middle text-center">10%</td>
+                                        <td class="p-1 align-middle text-center">5%</td>
+                                        <td class="p-1 align-middle text-center">8%</td>
+                                        <td class="p-1 align-middle text-center">8%</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Editing / Graphic Design</td>
+                                        <td class="p-1 align-middle text-center">13%</td>
+                                        <td class="p-1 align-middle text-center">8%</td>
+                                        <td class="p-1 align-middle text-center">13%</td>
+                                        <td class="p-1 align-middle text-center">10%</td>
+                                        <td class="p-1 align-middle text-center">13%</td>
+                                        <td class="p-1 align-middle text-center">15%</td>
+                                        <td class="p-1 align-middle text-center">8%</td>
+                                        <td class="p-1 align-middle text-center">10%</td>
+                                        <td class="p-1 align-middle text-center">10%</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
-                        <div class="card-footer">
-                            <button type="submit" class="btn btn-primary"><i class="fas fa-save mr-1"></i> Simpan</button>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-            <div class="col-md-8 col-12">
+            <div class="col-12">
                 <div class="card">
                     <div class="card-header">
                         <h3 class="card-title">List Kriteria</h3>
@@ -89,13 +133,6 @@
                                                     <div class="btn-group">
                                                         <a href="{{ route('kriteria.display',$row->id) }}" class="btn btn-info rounded mr-2"><i class="fa fa-eye mr-1"></i> Read</a>
                                                         <a href="{{ route('kriteria.edit',$row->id) }}" class="btn btn-warning rounded mr-2"><i class="fa fa-edit mr-1"></i> Edit</a>
-                                                        <form action="{{ route('kriteria.destroy', $row->id) }}" method="POST" class="delete-form">
-                                                            @csrf
-                                                            @method('DELETE')
-                                                            <button type="submit" class="btn btn-danger rounded hapus">
-                                                                <i class="fa fa-trash mr-1"></i> Hapus
-                                                            </button>
-                                                        </form>
                                                     </div>
                                                 </div>
                                             </td>
@@ -111,6 +148,13 @@
     </div>
     <script>
         $(function () {
+            $("#listpreset").DataTable({
+                "responsive": true, "lengthChange": false, "autoWidth": false,
+                "buttons": ["excel", "pdf", "print", "colvis"]
+            }).buttons().container().appendTo('#listpreset_wrapper .col-md-6:eq(0)');
+        });
+
+        $(function () {
             $("#kriteria").DataTable({
                 "responsive": true, "lengthChange": false, "autoWidth": false,
                 "buttons": ["excel", "pdf", "print", "colvis"]
@@ -118,59 +162,6 @@
         });
 
         $(document).ready(function () {
-            function attachDeleteEventListener() {
-                $('.hapus').off('click').on('click', function (event) {
-                    event.preventDefault();
-
-                    var deleteForm = $(this).closest('.delete-form');
-
-                    Swal.fire({
-                        title: "Apa kamu yakin?",
-                        text: "Sekali kamu hapus, data tidak dapat dikembalikan!",
-                        icon: "warning",
-                        showCancelButton: true,
-                        confirmButtonColor: "#3085d6",
-                        cancelButtonColor: "#d33",
-                        confirmButtonText: "Ya, hapus!",
-                        cancelButtonText: "Batal"
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            $.ajax({
-                                url: deleteForm.attr('action'),
-                                type: 'POST',
-                                data: {
-                                    '_method': 'DELETE',
-                                    '_token': "{{ csrf_token() }}"
-                                },
-                                success: function () {
-                                    Swal.fire({
-                                        title: "Berhasil menghapus data",
-                                        icon: "success",
-                                        showCancelButton: false,
-                                        confirmButtonColor: "#3085d6",
-                                        confirmButtonText: "OK"
-                                    }).then((result) => {
-                                        if (result.isConfirmed) {
-                                            window.location = "{{ route('kriteria.index') }}"
-                                        }
-                                    });
-                                }
-                            });
-                        } else {
-                            Swal.fire("Data aman", "", "info");
-                        }
-                    });
-
-                    return false;
-                });
-            }
-
-            attachDeleteEventListener();
-
-            $('body').on('DOMSubtreeModified', '.dataTables_paginate', function () {
-                setTimeout(attachDeleteEventListener, 100);
-            });
-
             @if(Session::has('msg'))
                 Swal.fire({
                     title: "{{ Session::get('msg') }}",
