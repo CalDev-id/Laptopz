de30106a02ca373f58209981ad57be75a8b008a5
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-07-26 09:06:23 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="/plugins/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="/dist/img/laptopz.png">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Laptopz | Login</title>
 </head>
 <body>
     <div class="bg-white relative">
         <div class="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-0 mr-auto mb-0 ml-auto max-w-7xl xl:px-5 lg:flex-row">
-        <div class="flex flex-col items-center w-full pt-5 pr-10 pb-20 lg:pt-20 lg:flex-row">
+        <div class="flex flex-col items-center w-full pt-5 pb-20 lg:pt-20 lg:flex-row">
             <div class="w-full bg-cover relative max-w-md lg:max-w-2xl lg:w-7/12">
                 <div class="flex flex-col items-center justify-center w-full h-full relative lg:pr-10">
                     <img src="dist/img/laptop.jpg" class="btn-"/>
