d11d8a944c79659c53a45d2c8889724a1f9d91aa
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-07-26 09:06:35 +0700

    Update index.blade.php

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,19 +3,25 @@
 @section('inner-content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-3 col-6">
-                <div class="small-box bg-info">
-                    <div class="inner">
-                        <h3>{{ $user }}</h3>
-                        <h4>User</h4>
+            @if(Auth::user()->roles == 'Administrator')
+                <div class="col-lg-3 col-12">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{ $user }}</h3>
+                            <h4>User</h4>
+                        </div>
+                        <div class="icon">
+                            <i class="ion fas fa-users"></i>
+                        </div>
+                        <a href="{{ route('user.index') }}" class="small-box-footer">More info ... <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
-                    <div class="icon">
-                        <i class="ion fas fa-users"></i>
-                    </div>
-                    <a href="{{ route('dashboard.index') }}" class="small-box-footer">More info ... <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
-            </div>
-            <div class="col-lg-3 col-6">
+            @endif
+            @if(Auth::user()->roles == 'Administrator')
+            <div class="col-lg-3 col-12">
+            @else
+            <div class="col-lg-4 col-12">
+            @endif
                 <div class="small-box bg-success">
                     <div class="inner">
                         <h3>{{ $kriteria }}</h3>
@@ -27,7 +33,11 @@
                     <a href="{{ route('kriteria.index') }}" class="small-box-footer">More info ... <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
-            <div class="col-lg-3 col-6">
+            @if(Auth::user()->roles == 'Administrator')
+            <div class="col-lg-3 col-12">
+            @else
+            <div class="col-lg-4 col-12">
+            @endif
                 <div class="small-box bg-warning">
                     <div class="inner">
                         <h3>{{ $subkriteria }}</h3>
@@ -39,7 +49,11 @@
                     <a href="{{ route('kriteria.index') }}" class="small-box-footer">More info ... <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
-            <div class="col-lg-3 col-6">
+            @if(Auth::user()->roles == 'Administrator')
+            <div class="col-lg-3 col-12">
+            @else
+            <div class="col-lg-4 col-12">
+            @endif
                 <div class="small-box bg-danger">
                     <div class="inner">
                         <h3>{{ $alternatif }}</h3>
